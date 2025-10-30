02b187d366d415810c68e78921f6a455f52c49a6
jrt <lucasjrt99@gmail.com>
2025-10-30 20:45:33 -0300

    Add orgId field to indexUserModel

--- incc_shared/models/email_index_user.py
+++ incc_shared/models/email_index_user.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, field_validator
 
 from incc_shared.models.user import Role
 
@@ -10,6 +10,16 @@ class EmailIndexUserModel(BaseModel):
     entity: str
     roles: List[Role] = [Role.USER]
     features: List[str] = []
+    orgId: str = ""
 
     gsi_email_pk: str
     gsi_user_sk: str
+
+    @field_validator("orgId", mode="after")
+    def validate_orgId(cls, v: str) -> str:
+        if not v:
+            try:
+                return cls.tenant.split("#")[1]
+            except IndexError:
+                raise ValidationError("Tenant must be on format ORG#{ID}")
+        return v
