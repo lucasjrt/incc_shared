f8a97766b821e8f0e13326dfe1c301ac44258b78
jrt <lucasjrt99@gmail.com>
2025-10-31 16:49:48 -0300

    Allow response with no body

--- incc_shared/utils/http.py
+++ incc_shared/utils/http.py
@@ -1,19 +1,25 @@
 import json
 
 
-def create_response(body, status_code=200, headers=None, isBase64Encoded=False):
+def create_response(body=None, status_code=200, headers=None, isBase64Encoded=False):
     """Generate a Lambda Proxy response with CORS headers."""
     cors_headers = {
+        # TODO: Improve CORS headers
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
         "Access-Control-Allow-Headers": "Authorization,Content-Type",
     }
+
     if headers:
         cors_headers.update(headers)
 
-    return {
+    payload = {
         "statusCode": status_code,
         "headers": cors_headers,
-        "body": body if isBase64Encoded else json.dumps(body, default=str),
         "isBase64Encoded": isBase64Encoded,
     }
+
+    if body:
+        payload["body"] = (body if isBase64Encoded else json.dumps(body, default=str),)
+
+    return payload
